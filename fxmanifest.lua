c5ea61f280c42bcba664996433118f8d56020d24
Breezy <63890993+DevBreezy@users.noreply.github.com>
2022-04-11 15:28:49 -0700

    Files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,9 @@
-fx_version 'cerulean'
-games { 'rdr3', 'gta5' }
-
 name 'BreezyStaffChat'
 description 'Staff Chat resource.'
 author 'Breezy#0001'
 version "1.3"
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
 
 server_script 'server.lua'
 
