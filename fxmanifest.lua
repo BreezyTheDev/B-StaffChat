9ed44eb52002df923a2f78cd9cfcdf9c1f78db02
Breezy <63890993+BreezyTheDev@users.noreply.github.com>
2022-05-06 17:50:54 -0700

    B-StaffChat

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+author "Creator: BreezyTheDev | Discord: Breezy#0001"
+description "B-StaffChat Vesrion 1.3"
+version "1.3"
+
+fx_version "cerulean"
+game "gta5"
+
+
+server_script 'server.lua'
+
+client_script 'client.lua'
+
