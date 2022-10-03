82a8621d4cc2a739a78553e6eb91f644f84bb85b
BreezyTheDev <63890993+BreezyTheDev@users.noreply.github.com>
2022-10-03 13:52:28 -0700

    BreezyStaffChat 2.0

--- server.lua
+++ server.lua
@@ -1,97 +1,106 @@
 --================================--
---      B-StaffChat 1.0           --
---      by BreezyTheDev           --
---		GNU License v3.0		  --
+--      BreezyStaffChat 2.0       --
+--      by DevBreezy              --
+--		MIT License				  --
 --================================--
 
--- Configuration
-prefix = '^3[^1Staff Chat^3] '
+function GetAllPlayers()
+    local players = {}
 
--- Commands
+    for _, i in ipairs(GetPlayers()) do
+        table.insert(players, i)    
+    end
 
+    return players
+end
+
+local displaystaffchat = false
 RegisterCommand("sc", function(source, args, rawCommand)
+    local player = source
+    local name = GetPlayerName(player)
+    local message = table.concat(args, " ")
     if IsPlayerAceAllowed(source, "StaffChat.View") then
-        local src = source
-        local message = table.concat(args, " ")
-        local name = GetPlayerName(source)
-            ifMessage = false
-            if #args > 0 then
-                ifMessage = true
+        if displaystaffchat == false then
+            if message ~= "" then
+                local players = GetAllPlayers()
+                for i=1, #players do
+                    if IsPlayerAceAllowed(players[i], "StaffChat.View") then
+                        if Config.Template then
+                            TriggerClientEvent("chat:addMessage", players[i], {
+                                color = {255, 0, 0},
+                                template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(186, 51, 187, 0.6); border-radius: 3px;"><i class="fas fa-shield-alt"></i> {0} {1}</div>',
+                                args = {"^0" .. "[" .. source .. "] " .. name .. ":" , message}
+                            })
+                        else
+                            TriggerClientEvent('chat:addMessage', players[i], {args = {Config.Prefix .. "^5" .. name ..": ^1"..message}})
+                        end
+                    end
+                end
             else
-                ifMessage = false
+                if Config.Template then
+                    TriggerClientEvent("chat:addMessage", player, {
+                        color = {255, 0, 0},
+                        template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(186, 51, 187, 0.6); border-radius: 3px;"><i class="fas fa-shield-alt"></i> {0} </div>',
+                        args = {"^0You must include a message!"}
+                    })
+                else
+                    TriggerClientEvent('chat:addMessage', player, {args = {Config.Prefix  .. "^5 You must include a message!"}})
+                end
             end
-        local staffmessage = "^5 " .. table.concat(args, " ")
-        TriggerClientEvent("B-StaffChat:SendToStaffClient", -1, staffmessage, src, ifMessage)
+        elseif displaystaffchat == true then
+            if Config.Template then
+                TriggerClientEvent("chat:addMessage", player, {
+                    color = {255, 0, 0},
+                    template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(186, 51, 187, 0.6); border-radius: 3px;"><i class="fas fa-shield-alt"></i> {0} </div>',
+                    args = {"^0Your staff chat is toggled off, toggle it back on to use it again."}
+                })
+            else
+                TriggerClientEvent('chat:addMessage', player, {args = {Config.Prefix  .. "^5 Your staff chat is toggled off, toggle it back on to use it again."}})
+            end
+        end
     else
-        TriggerClientEvent('chatMessage', source, prefix .. 'You do not have permission to do this!')
-    end
-end)
-
--- Events 
-
-RegisterNetEvent("B-StaffChat:SendToStaff")
-AddEventHandler("B-StaffChat:SendToStaff", function(message, id, checkMessage)
-	local _src = source
-	local name = GetPlayerName(id)
-	if IsPlayerAceAllowed(_src, "StaffChat.View") then
-		if checkMessage then
-			TriggerClientEvent('chat:addMessage', _src, {args = {prefix .. "^3" .. name ..":"..message}})
-		end
-	end
-end)
-
-
-RegisterServerEvent("B-StaffChat:checkPerms")
-AddEventHandler("B-StaffChat:checkPerms", function()
-    if IsPlayerAceAllowed(source, "StaffChat.View") then
-        TriggerClientEvent("B-StaffChat:returnPerms", source, true)
-    else
-        TriggerClientEvent("B-StaffChat:returnPerms", source, false)
-    end
-end)
-
--- Functions
-
-local function has_value (tab, val)
-    for index, value in ipairs(tab) do
-        if value == val then
-            return true
+        if Config.Template then
+            TriggerClientEvent("chat:addMessage", player, {
+                color = {255, 0, 0},
+                template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(186, 51, 187, 0.6); border-radius: 3px;"><i class="fas fa-shield-alt"></i> {0} </div>',
+                args = {"^0You need permission to run this command!"}
+            })
+        else
+            TriggerClientEvent('chat:addMessage', player, {args = {Config.Prefix  .. "^5 You need permission to run this command!"}})
         end
     end
+end, false)
 
-    return false
-end
-function sleep (a) 
-    local sec = tonumber(os.clock() + a); 
-    while (os.clock() < sec) do 
-    end 
-end
-function stringsplit(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t={} ; i=1
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-        t[i] = str
-        i = i + 1
-    end
-    return t
-end
-function get_index (tab, val)
-	local counter = 1
-    for index, value in ipairs(tab) do
-        if value == val then
-            return counter
+
+RegisterCommand("sc-toggle", function(source, args, rawCommand)
+    local player = source
+    if displaystaffchat == false then
+        displaystaffchat = true
+        if Config.Template then
+            TriggerClientEvent("chat:addMessage", player, {
+                color = {255, 0, 0},
+                template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(186, 51, 187, 0.6); border-radius: 3px;"><i class="fas fa-shield-alt"></i> {0} </div>',
+                args = {"^0You will no longer receive new messages from Staff Chat."}
+            })
+        else
+            TriggerClientEvent('chat:addMessage', player, {args = {Config.Prefix  .. "^5 You will no longer receive new messages from Staff Chat."}})
         end
-		counter = counter + 1
+    elseif displaystaffchat == true then
+        displaystaffchat = false
+        if Config.Template then
+            TriggerClientEvent("chat:addMessage", player, {
+                color = {255, 0, 0},
+                template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(186, 51, 187, 0.6); border-radius: 3px;"><i class="fas fa-shield-alt"></i> {0} </div>',
+                args = {"^0You will now receive new messages from Staff Chat."}
+            })
+        else
+            TriggerClientEvent('chat:addMessage', player, {args = {Config.Prefix  .. "^5 You will now receive new messages from Staff Chat."}})
+        end
     end
+end, false)
 
-    return nil
-end
 
--- Version Checker
-
-expectedName = "B-StaffChat" -- This is the resource and is not suggested to be changed.
+expectedName = "BreezyStaffChat" -- This is the resource and is not suggested to be changed.
 resource = GetCurrentResourceName()
 
 -- check if resource is renamed
@@ -101,21 +110,21 @@ if resource ~= expectedName then
 end
 
 
-print("^0This resource was created by ^5Breezy#0001 ^0for support you can join his ^5discord: ^0https://discord.gg/zzUfkfRHzP")
+print("^0This resource is created by ^5Breezy#0001 ^0for support you can join the ^5discord: ^0https://discord.gg/zzUfkfRHzP")
 
 -- check if resource version is up to date
-PerformHttpRequest("https://raw.githubusercontent.com/BreezyTheDev/B-StaffChat/main/fxmanifest.lua", function(error, res, head)
+PerformHttpRequest("https://raw.githubusercontent.com/DevBreezy/BreezyStaffChat/main/fxmanifest.lua", function(error, res, head)
     i, j = string.find(tostring(res), "version")
     res = string.sub(tostring(res), i, j + 6)
     res = string.gsub(res, "version ", "")
+
     res = string.gsub(res, '"', "")
     resp = tonumber(res)
     verFile = GetResourceMetadata(expectedName, "version", 0)
-    
     if verFile then
         if tonumber(verFile) < resp then
             print("^1[^4" .. expectedName .. "^1] WARNING^0")
-            print("^4" .. expectedName .. " ^0is outdated. Please update it from ^5https://github.com/BreezyTheDev/B-StaffChat^0| Current Version: ^1" .. verFile .. " ^0| New Version: ^2" .. resp .. " ^0|")
+            print("^4" .. expectedName .. " ^0is outdated. Please update it from ^5https://github.com/DevBreezy/BreezyStaffChat^0| Current Version: ^1" .. verFile .. " ^0| New Version: ^2" .. resp .. " ^0|")
         elseif tonumber(verFile) > tonumber(resp) then
             print("^1[^4" .. expectedName .. "^1] WARNING^0")
             print("^4" .. expectedName .. "s ^0version number is higher than we expected. | Current Version: ^3" .. verFile .. " ^0| Expected Version: ^2" .. resp .. " ^0|")
@@ -124,6 +133,6 @@ PerformHttpRequest("https://raw.githubusercontent.com/BreezyTheDev/B-StaffChat/m
         end
     else
         print("^1[^4" .. expectedName .. "^1] WARNING^0")
-        print("You may not have the latest version of ^4" .. expectedName .. "^0. A newer, improved version may be present at ^5https://github.com/BreezyTheDev/B-StaffChat^0")
+        print("You may not have the latest version of ^4" .. expectedName .. "^0. A newer, improved version may be present at ^5https://github.com/DevBreezy/BreezyStaffChat^0")
     end
 end)
