29546c25e9871c70184fe27ebcffc9442b75b060
BreezyTheDev <63890993+BreezyTheDev@users.noreply.github.com>
2022-10-03 13:53:29 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,9 @@
 author "Creator: BreezyTheDev | Discord: Breezy#0001"
 description "B-StaffChat Vesrion 1.3"
-version "1.3"
+version "2.0"
 
 fx_version "cerulean"
 game "gta5"
-
 
 server_script 'server.lua'
 
