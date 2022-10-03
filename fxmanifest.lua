a96b73a952bc8b8acd09d5b8eb4b179d6df7fe1e
BreezyTheDev <63890993+BreezyTheDev@users.noreply.github.com>
2022-10-03 13:54:34 -0700

    BreezyStaffChat 2.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,9 @@ version "2.0"
 fx_version "cerulean"
 game "gta5"
 
-server_script 'server.lua'
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
 
-client_script 'client.lua'
-
+file 'style.css'
