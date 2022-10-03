e5b3f8347c2610bcfbed4151c399447f8cbfb0c2
BreezyTheDev <63890993+BreezyTheDev@users.noreply.github.com>
2022-10-03 13:53:02 -0700

    BreezyStaffChat 2.0

---
+++ config.lua
@@ -0,0 +1,11 @@
+--================================--
+--      BreezyStaffChat 1.0       --
+--      by DevBreezy              --
+--		MIT License				  --
+--================================--
+
+Config = {
+	Template = true -- I use a custom template for this resource, set to false if you would like to use the default chat message template.
+    Prefix = '^5[^1Staff Chat^5] ', -- This is only used if Template is set to false.
+}
+
